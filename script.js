3993a2df717efb2cd480b58bdef8cb2282f3e795
Ethan May <ethaniel.may@gmail.com>
2022-05-23 21:46:49 -0600

    Functions

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function possess() {
     document.getElementById("img2").classList.remove("hidden")
+    document.getElementById("img").classList.add("hidden")
 }
+
+function unpossess() {
+    document.getElementById("img").classList.remove("hidden")
+    document.getElementById("img2").classList.add("hidden")
+}
+
