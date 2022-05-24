a19328d6b25f34462499122becf9138c37862237
Ethan May <ethaniel.may@gmail.com>
2022-05-23 21:47:10 -0600

    Toggle Possession

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ function unpossess() {
     document.getElementById("img2").classList.add("hidden")
 }
 
+function togglePossess() {
+    if (document.getElementById("img").classList.contains("hidden")) { // If possessed      
+        document.getElementById("img").classList.remove("hidden")
+        document.getElementById("img2").classList.add("hidden")
+    } else if (document.getElementById("img2").classList.contains("hidden")) { // If Unpossessed
+        document.getElementById("img2").classList.remove("hidden")
+        document.getElementById("img").classList.add("hidden")
+    } else {
+        window.alert("An error with possession occurred.")
+    }
+}
